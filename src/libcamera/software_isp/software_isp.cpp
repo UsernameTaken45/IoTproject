b5d86874cf40c4248f0f07671870037c9fe120f7
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2025-08-24 01:48:28 +0100

    libcamera: software_isp: debayer: Introduce a stop() callback to the debayer object
    The eGL class wants to be able to teardown its sync_ data member
    properly but, doing so in the destructor means we can't make the eGL
    context current and thus can't tear down the sync primitive properly.
    
    Introduce a stop() method to the debayer class which triggers from the
    softisp's stop method, allowing a controlled and appropriate tear-down
    of debayer-egl and egl class related data well before the destructors
    get invoked.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- src/libcamera/software_isp/software_isp.cpp
+++ src/libcamera/software_isp/software_isp.cpp
@@ -363,6 +363,9 @@ int SoftwareIsp::start()
  */
 void SoftwareIsp::stop()
 {
+	debayer_->invokeMethod(&Debayer::stop,
+			       ConnectionTypeQueued);
+
 	ispWorkerThread_.exit();
 	ispWorkerThread_.wait();
 
