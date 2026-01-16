61a2f40f7e24df956645a647a47cf5eefd40142d
projectgroep <22081631@student.hhs.nl>
2026-01-16 13:28:46 +0100

    Added check in ccm.cpp for simple SoftISP to check if it read a color temp of 0 from config

--- src/ipa/simple/algorithms/ccm.cpp
+++ src/ipa/simple/algorithms/ccm.cpp
@@ -89,6 +89,10 @@ void Ccm::prepare(IPAContext &context, const uint32_t frame,
 	auto &saturation = context.activeState.knobs.saturation;
 
 	const unsigned int ct = context.activeState.awb.temperatureK;
+	if (ct == 0) {
+		LOG(IPASoftCcm, Warning) << "Ccm value read from config Yaml is 0. ";
+	}
+
 	/* Change CCM only on saturation or bigger temperature changes. */
 	if (frame > 0 &&
 	    utils::abs_diff(ct, lastCt_) < kTemperatureThreshold &&
